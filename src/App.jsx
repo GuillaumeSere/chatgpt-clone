3574925a042f15b5211b9077dba0233d0b3c1fd1
Guillaume Sere <guillaumesere@hotmail.fr>
2023-02-14 11:50:22 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import ChatGpt from './components/ChatGpt';
+import GenerateImage from './components/GenerateImage';
+import Header from './components/Header';
+
+
+function App() {
+
+
+    return (
+        <>
+        <Header />
+        <div className='App'>
+            <Routes>
+                <Route path="/chatgpt" element={ <ChatGpt />} />
+                <Route path="/images" element={ <GenerateImage />} />
+            </Routes>
+        </div>
+        </>
+    )
+}
+
+export default App;
